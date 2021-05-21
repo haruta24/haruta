3e0f5acb094b72d9a0f8853cbb9c5bc8c12f42ab
haruta24 <mptjapw@gmail.com>
2021-05-21 09:39:30 +0900

    update

---
+++ pages/apps/text.js
@@ -0,0 +1,36 @@
+import { Button, Box, Text, HStack } from "@chakra-ui/react"
+import { useEffect, useState } from "react"
+import Head from "next/head"
+import Link from "next/link"
+import { BoxHeader } from "../../components/BoxHeader"
+import { Textarea } from "@chakra-ui/react"
+
+const TextApp = () => {
+  const [text, setText] = useState("")
+
+  return (
+    <>
+      <Head>
+        <title>{"text"}</title>
+      </Head>
+      <BoxHeader />
+      <Box p={4}>
+        <Textarea
+          placeholder={"Here is a sample placeholder"}
+          value={text}
+          onChange={(event) => {
+            // 新しい入力
+            const nextText = event.target.value
+            // 20文字より大きいと更新しない
+            if (nextText.length > 20) return
+            // 入力を更新する
+            setText(nextText)
+          }}
+        />
+        <Text>{`${text.length}/20`}</Text>
+      </Box>
+    </>
+  )
+}
+
+export default TextApp
--- pages/index.js
+++ pages/index.js
@@ -15,24 +15,36 @@ import { BoxHeader } from "../components/BoxHeader"
 import { BoxCardApp } from "../components/BoxCardApp"
 
 const Home = () => {
+  const apps = [
+    {
+      href: "/apps/click",
+      title: "クリック",
+      description: "クリックするとカウントが増える",
+    },
+    {
+      href: "/apps/clock",
+      title: "時計",
+      description: "時刻を表示する",
+    },
+    {
+      href: "/apps/text",
+      title: "入力",
+      description: "文字を入力する",
+    },
+  ]
+
   return (
     <>
       <BoxHeader />
       <Stack as={"main"} p={4} spacing={16}>
         <SimpleGrid minChildWidth={"280px"} spacing={10}>
-          <Link href={"/apps/counter"}>
-            <a>
-              <BoxCardApp
-                title={"カウンター"}
-                description={"クリックするとカウントが増える"}
-              />
-            </a>
-          </Link>
-          <Link href={"/apps/clock"}>
-            <a>
-              <BoxCardApp title={"時計"} description={"時刻を表示する"} />
-            </a>
-          </Link>
+          {apps.map((app) => (
+            <Link href={app.href} key={app.href}>
+              <a>
+                <BoxCardApp title={app.title} description={app.description} />
+              </a>
+            </Link>
+          ))}
         </SimpleGrid>
       </Stack>
     </>
